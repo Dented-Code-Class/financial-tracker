78dcf8c06817c2756006518d8a88ca16c6073bfd
Luffy-28 <thakurishankar159@gmail.com>
2026-03-01 11:02:23 +1100

    create CRUD apis for transactions

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,13 @@ import mongoose, { mongo } from "mongoose";
 import { configDotenv } from "dotenv";
 import cors from "cors";
 import { createUser } from "./src/controllers/userControllers.js";
+import {
+  createTransaction,
+  deleteTransaction,
+  deleteTransactions,
+  getTransaction,
+  updateTransaction,
+} from "./src/controllers/transactionControllers.js";
 configDotenv();
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -14,6 +21,21 @@ app.use(cors());
 
 //User Register
 app.post("/api/v1/auth/register", createUser);
+
+//Create Transaction
+app.post("/api/v1/transactions", createTransaction);
+
+//Get Transaction
+app.get("/api/v1/transactions", getTransaction);
+
+//Update Transaction
+app.patch("/api/v1/transactions/:id", updateTransaction);
+
+//Delete Transaction by ID
+app.delete("/api/v1/transactions/:id", deleteTransaction);
+
+//Delete Transaction by IDs
+app.delete("/api/v1/transactions", deleteTransactions);
 
 mongoose
   .connect(MONGO_URL) // MONGOURL --> Connection string
---
+++ backend/src/controllers/transactionControllers.js
@@ -0,0 +1,97 @@
+import { Transaction } from "../models/transactionModel.js";
+
+export const createTransaction = async (req, res) => {
+  try {
+    const newTransaction = req.body;
+    const data = await Transaction.insertOne(newTransaction);
+    return res.send({
+      status: "success",
+      messgae: "Transaction added succesfully",
+      Transaction: data,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.send({
+      status: "error",
+      message: "Error Adding Transactionr",
+    });
+  }
+};
+
+export const getTransaction = async (req, res) => {
+  try {
+    const transactions = await Transaction.find();
+    return res.send({
+      status: "success",
+      message: "Transaction found",
+      transactions,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.send({
+      status: "error",
+      message: "Error getting transaction",
+    });
+  }
+};
+
+export const updateTransaction = async (req, res) => {
+  try {
+    const transactionId = req.params.id;
+    const updatedPayload = req.body;
+    const transaction = await Transaction.findByIdAndUpdate(
+      transactionId,
+      updatedPayload,
+      {
+        new: true,
+      },
+    );
+    return res.send({
+      status: "success",
+      message: "Update successful",
+      transaction,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.send({
+      status: "error",
+      message: "Error updating transaction",
+    });
+  }
+};
+
+export const deleteTransaction = async (req, res) => {
+  try {
+    const trasactionId = req.params.id;
+    const data = await Transaction.deleteOne({ _id: trasactionId });
+    return res.send({
+      status: "success",
+      message: "Delete transaction successful",
+      data,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.send({
+      status: "error",
+      message: "Error deleting transaction",
+    });
+  }
+};
+
+export const deleteTransactions = async (req, res) => {
+  try {
+    const transactionIds = req.body.ids;
+    let data = await Transaction.deleteMany({ _id: { $in: transactionIds } });
+    return res.send({
+      status: "success",
+      message: "selected transaction deleted",
+      data,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.send({
+      status: "error",
+      message: "error deleting transactions",
+    });
+  }
+};
